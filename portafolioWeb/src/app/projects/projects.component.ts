898e58787e94be77d4458750a7a3637d0d163e55
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-06-28 17:44:49 -0500

    Projects Component
    Gran parte del componente de proyectos realizado

--- portafolioWeb/src/app/projects/projects.component.ts
+++ portafolioWeb/src/app/projects/projects.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project';
+import { ProjectService } from '../services/project.service';
 
 @Component({
-  selector: 'app-projects',
+  selector: 'projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  public titulo: string = "Proyectos";
+  public descripcion: string = "A continuación algunos de mis proyectos realizados a lo largo de mi formación academica"
+  public projects: Array<Project> | undefined;
+  constructor(
+    private _projectService: ProjectService
+  ) { }
+
+
+
+  ngOnInit(): void {
+    this.projects = this._projectService.getProjects();
+  }
 
 }
---
+++ portafolioWeb/src/app/services/project.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { Project } from "../models/project";
+
+@Injectable()
+export class ProjectService {
+    public projects: Array<Project>;
+    constructor() {
+        this.projects = [
+            new Project('InteNews', 'Colaboración en el diseño y desarrollo FrontEnd para un portal web encargado de realizar WebScraping, donde se realiza recolección de noticias naciona les (Colombia).', '', 'https://soshace.com/wp-content/uploads/2019/09/responsible-web-scraping-gathering-data-ethically-facebook-cover-image.jpg', ['HTML5', 'CSS3', 'js', 'Sass']),
+            new Project('Luxua', 'Desarrollo en conjunto de un sistema CRM/ERP, con características de manejo de inventario, privilegios para distintos roles de usuarios, sistema de facturación.', '', 'https://www.dongee.com/tutoriales/content/images/2022/11/image-82.png', ['HTML5', 'CSS3-alt', 'php']),
+            new Project('FoodHub', 'Desarrollo frontend de aplicación móvil utilizando Flutter y Dart, enfocado en interfaces intuitivas y funcionales y colaborando estrechamente con el equipo de desarrollo para una experiencia de usuario fluida y atractiva.', '', 'https://soshace.com/wp-content/uploads/2019/09/responsible-web-scraping-gathering-data-ethically-facebook-cover-image.jpg', ['HTML5', 'CSS3-alt', 'js'])
+        ]
+    }
+
+    getProjects(): Array<Project> {
+        return this.projects;
+    }
+}
