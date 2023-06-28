326fa33f779c0256b95ace806668caa125e58f5d
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-06-28 14:44:52 -0500

    Componente home realizado
    (Sujeto a cambios)

--- portafolioWeb/src/app/app.module.ts
+++ portafolioWeb/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProvider } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ portafolioWeb/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders } from "@angular/core"; //modulo interno de angular para trabajar con las rutas 
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { AboutComponent } from "./about/about.component";
+
+const appRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: HomeComponent },
+];
+
+export const appRoutingProvider: any[] = [];
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes);
