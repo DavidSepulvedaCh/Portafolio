7dfaebfab789823f4096004e8e1e7c906f5be353
David Leonardo Sepúlveda Chávez <116600515+DavidSepulvedaCh@users.noreply.github.com>
2023-09-14 23:53:14 -0500

    Actualizaciones de diseño
    Se actualiza el componente about, se actualizo la imagen, también cambios en el componente de proyectos

--- portafolioWeb/src/app/services/skills.service.ts
+++ portafolioWeb/src/app/services/skills.service.ts
@@ -8,13 +8,15 @@ export class SkillService {
         this.skills = [
             new Skill('HTML5', '100', 'Lenguaje'),
             new Skill('CSS3-alt', '100', 'Lenguaje'),
+            new Skill('Sass', '100', 'Lenguaje'),
             new Skill('JS', '80', 'Lenguaje'),
             new Skill('Java', '80', 'Lenguaje'),
             new Skill('SQL', '80', 'Lenguaje'),
             new Skill('PHP', '80', 'Lenguaje'),
+            new Skill('Python', '', 'Lenguaje'),
             new Skill('Angular', '100', 'Framework'),
             new Skill('Bootstrap', '100', 'Framework'),
-            new Skill('React', '50', 'Framework')
+            new Skill('Django', '', 'Framework'),
         ];
     }
 
